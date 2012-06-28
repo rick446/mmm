5f4e3068bebeb325201a8fba62323b67c4d7b2b1
Rick Copeland <rick@arborian.com>
2012-06-28 14:58:04 -0400

    Updates to setup.py, added LICENSE and NOTICE files
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- setup.py
+++ setup.py
@@ -1,27 +1,36 @@
 from setuptools import setup, find_packages
-import sys, os
+import os
 
 version = '0.0'
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fp:
+    long_description = fp.read()
 
 setup(name='MongoMultiMaster',
       version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='',
-      author_email='',
+      description="Multimaster replication for MongoDB",
+      long_description=long_description,
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database :: Database Engines/Servers',
+        'Topic :: Utilities' ],
+      keywords='mongodb,replication',
+      author='Rick Copeland',
+      author_email='rick@arborian.com',
       url='',
-      license='',
+      license='Apache License, Version 2.0',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+        'gevent',
+        'pymongo',
+        'pyyaml',
       ],
       scripts=['scripts/mmm'],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+      entry_points="")
